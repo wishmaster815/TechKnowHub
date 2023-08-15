3939773faee34a59e6d89505ffb5d4a68a071480
Haider <haidersoni47@gmail.com>
2023-08-15 22:16:00 +0530

    page 1 scroll rem

--- script.js
+++ script.js
@@ -8,38 +8,3 @@ tl.from("#imgs h2,#imgs img,#page2Txt h2,#page2Txt p,#page2Txt button",{
     
 })
 
-// gsap.to("#imgs img",{
-//     y:-100,
-//     duration:1,
-    
-//     // stagger:0.3,
-//     // opacity:0,
-//     // markers:true,
-//     scrollTrigger:{
-//         markers:true,
-//         trigger:"#page2Txt button",
-//         scroller:"body",
-        
-//         start:"top 80%",
-//         end:"top 50%",
-//         scrub:2
-//     }
-// })
-
-gsap.to("#page2",{
-    y:-200,
-    duration:1,
-    stagger:0.2,
-    
-    opacity:0,
-    // markers:true,
-    scrollTrigger:{
-        markers:true,
-        trigger:"#page2Txt button",
-        scroller:"body",
-        
-        start:"top 80%",
-        end:"top 20%",
-        scrub:2
-    }
-})
