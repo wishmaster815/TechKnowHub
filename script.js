1d297628d73f0743121e137cdc540b4e0a87548e
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2023-08-16 13:57:11 +0530

    haider ne jayesh ke lappy se

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var tl = gsap.timeline();
 tl.from("#imgs h2,#imgs img,#page2Txt h2,#page2Txt p,#page2Txt button",{
     y:100,
-    duration:1,
-    delay:0.5,
+    
+    // delay:0.5,
     opacity:0,
     stagger:0.2,
+    scrollTrigger:{
+        scroller:"body",
+        trigger:"#page2",
+        // start:"top 20%",
+        end:"top 40%",
+        markers:true,
+        scrub:2
+    }
     
 })
 
